7abeba33ac10f611256c9b8672a8baa9d20c43e0
Adm <anton.chubshev@yandex.ru>
2024-07-22 16:46:54 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,43 @@
+def get_mask_card_number(user_input_card: list) -> str:
+    """Функция принимает в качестве аргумента номер карты и возвращает его маску"""
+
+    # Разбиваем номер карты на список из 4 элементов
+    mask = [user_input_card[i : i + 4] for i in range(0, len(user_input_card), 4)]
+
+    # Заменяем цифры в монере карты на знак *
+    for i, v in enumerate(mask[1]):
+        if i > 1:
+            mask[1][i] = "*"
+
+    # Заменяем цифры в монере карты на знак *
+    for i, v in enumerate(mask[2]):
+        mask[2][i] = "*"
+
+    # Собираем замаскированный номер карты
+    mask_card_number = "".join(mask[0]) + " " + "".join(mask[1]) + " " + "".join(mask[2]) + " " + "".join(mask[3])
+    return mask_card_number
+
+
+def get_mask_account(user_input_account: list) -> str:
+    """Функция принимает в качестве аргумента номер счета и возвращает его маску"""
+
+    # Берем последние 6 цифр из номера счета для формирования маски
+    new_mask_account = user_input_account[-6::]
+
+    # Замена первых двух символов на знак *
+    for i, v in enumerate(new_mask_account):
+        if i <= 1:
+            new_mask_account[i] = "*"
+
+    # Преобразоввываем получившийся списко в строку для формирования окончательного вида маски
+    mask_account = "".join(new_mask_account)
+
+    return mask_account
+
+
+user_input_card = list(input("Введите номер карты: "))
+
+user_input_account = list(input("Введите номер счета: "))
+
+print(get_mask_card_number(user_input_card))
+print(get_mask_account(user_input_account))
---
+++ src/widget.py
@@ -0,0 +1,62 @@
+def mask_account_card(user_input: str) -> str:
+    """Функция маскирует номер карты или номер счета в зависимости от полученного аргумента"""
+
+    new_card_name = ""
+    card_numbers = ""
+
+    # Проверка на введенный аргумент. Маскировка счета, если аргумент - счет. Подготовка к маскировки карты, если аргумент - карта
+    for i in user_input:
+        if i == "С":
+            new_account_name = user_input[:5]
+            new_account_numbers = user_input[-4:]
+            mask_account = new_account_name + "**" + new_account_numbers
+        elif i != "С":
+            if i.isalpha() or i == " ":
+                new_card_name += i
+            elif i.isdigit():
+                card_numbers += i
+
+    # Разбиваем номер карты на список из 4 элементов
+    card_numbers_list = [card_numbers[i : i + 4] for i in range(0, len(card_numbers), 4)]
+
+    mask_card_numbers = " ".join(card_numbers_list[1]) + " " + " ".join(card_numbers_list[2])
+
+    mask_card_numbers_list = []
+
+    for i in mask_card_numbers:
+        if i != " ":
+            mask_card_numbers_list.append(i)
+
+    for i, v in enumerate(mask_card_numbers_list):
+        if i >= 2:
+            mask_card_numbers_list[i] = "*"
+
+    mask_card = (
+        new_card_name
+        + card_numbers_list[0]
+        + " "
+        + "".join(mask_card_numbers_list[0:4])
+        + " "
+        + "".join(mask_card_numbers_list[4::])
+        + " "
+        + card_numbers_list[3]
+    )
+
+    # Возврат работы функции в зависимости от полученного аргумента
+    for i in user_input:
+        if i == "С":
+            return mask_account
+        elif i != "С":
+            return mask_card
+
+
+def get_date(user_date: str) -> str:
+    """Функция принимает в качестве аргумента строку с датой и возвращает эту дату в другом формате"""
+
+    new_date = "".join(user_date[8:10]) + "-" + "".join(user_date[5:7]) + "-" + "".join(user_date[:4])
+
+    return new_date
+
+
+print(mask_account_card(user_input="Maestro 8990922113665229"))
+print(get_date(user_date="2024-07-20T02:26:18.671407"))
