807b502a28188e0bc511c8a2a203c76193524398
Adm <anton.chubshev@yandex.ru>
2024-07-24 11:01:28 +0300

    func filter_by_card

--- src/processing.py
+++ src/processing.py
@@ -11,14 +11,6 @@ def filter_by_state(filterable_list: list, state: str = "EXECUTED") -> list:
     return new_list
 
 
-def sort_by_date(sortable_list: list, sorter: bool = True) -> list:
-    """Сортирует заданный список по дате. По умолчанию список сортируется по убыванию"""
-
-    sorted_list = sorted(sortable_list, key=lambda x: x["date"], reverse=sorter)
-
-    return sorted_list
-
-
 print(
     filter_by_state(
         filterable_list=[
@@ -28,16 +20,5 @@ print(
             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         ],
         state="EXECUTED",
-    )
-)
-
-print(
-    sort_by_date(
-        sortable_list=[
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
     )
 )
