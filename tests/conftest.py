c8fd35d0d5a8e691c0ad5f37ebfe38f2de29b811
Adm <anton.chubshev@yandex.ru>
2024-08-06 10:40:37 +0300

    add fixture for masks.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,10 @@ import pytest
 
 @pytest.fixture
 def card_numb():
-    return ['1234 12** **** 1234', [], 'Некорректный ввод']
+    return '1234 12** **** 1234'
 
 
+@pytest.fixture
 def acc_numb():
-    return ['**8912', [], 'Некорректный ввод']
-
+    return '**8912'
 
