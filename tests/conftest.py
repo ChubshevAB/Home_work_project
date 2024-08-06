448750ef839fd6d1bfe47c1c8eb3c885b986d0f9
Adm <anton.chubshev@yandex.ru>
2024-08-06 19:28:01 +0300

    add fixtures for all functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,42 @@ import pytest
 
 @pytest.fixture
 def card_numb():
-    return '1234 12** **** 1234'
+    return "1234 12** **** 1234"
 
 
 @pytest.fixture
 def acc_numb():
-    return '**8912'
+    return "**8912"
 
+
+@pytest.fixture
+def widget_card():
+    return "Maestro 1234 12** **** 1234"
+
+
+@pytest.fixture
+def widget_acc():
+    return "Счет **1234"
+
+
+@pytest.fixture
+def widget_data():
+    return "20-07-2024"
+
+
+@pytest.fixture
+def processing_filter_by_state():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+@pytest.fixture
+def processing_sort_by_date():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
