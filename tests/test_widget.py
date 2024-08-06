88996182929be641c247acf301b36633d25fe5ca
Adm <anton.chubshev@yandex.ru>
2024-08-06 19:30:27 +0300

    add parametrize for tests

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,11 +1,29 @@
+import pytest
+
+
 from src.widget import mask_account_card, get_date
 
 
-def test_mask_card(widget_card):
-    assert mask_account_card(list('Maestro 1234123412341234')) == widget_card
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (list("Maestro 1234123412341234"), "Maestro 1234 12** **** 1234"),
+        (list("Master Card 1234123412341234"), "Master Card 1234 12** **** 1234"),
+    ],
+)
+def test_mask_card(value, expected):
+    assert mask_account_card(value) == expected
 
-def test_mask_acc(widget_acc):
-    assert mask_account_card('Счет 1234123412341234') == widget_acc
 
-def test_get_data(widget_data):
-    assert get_date('2024-07-20T02:26:18.671407') == widget_data
+@pytest.mark.parametrize(
+    "value, expected", [("Счет 1234123412341234", "Счет **1234"), ("Счет 98765432198765432198", "Счет **2198")]
+)
+def test_mask_acc(value, expected):
+    assert mask_account_card(value) == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected", [("2024-07-20T02:26:18.671407", "20-07-2024"), ("2023-01-17T03:15:48.487193", "17-01-2023")]
+)
+def test_get_data(value, expected):
+    assert get_date(value) == expected
