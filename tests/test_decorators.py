bee4cb1597900e6818f7a26dec0561fb201f9742
Adm <anton.chubshev@yandex.ru>
2024-08-09 19:16:14 +0300

    fix

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,4 +6,4 @@ from src.decorators import log, func
 def test_log(capsys):
     func(1, 2)
     captured = capsys.readouterr()
-    assert captured.out == 'my_function ok\n'
+    assert captured.out == "my_function ok\n"
