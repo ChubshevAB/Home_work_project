24e349608eb55be97f6c65a7f4b0c67e6d58d7e7
Adm <anton.chubshev@yandex.ru>
2024-08-09 19:15:31 +0300

    fix

--- src/decorators.py
+++ src/decorators.py
@@ -2,35 +2,37 @@ from functools import wraps
 
 
 def log(filename=None):
+    """Функция-декоратор логирует работу функции и выводит сообщение о успешной работе либо об ошибке в консоль либо в файл, если указать имя файла"""
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
                 if filename:
-                    with open(filename, 'a', encoding='utf-8') as file:
+                    with open(filename, "a", encoding="utf-8") as file:
                         file.write(f"my_function ok\n")
                 else:
                     print(f"my_function ok")
 
             except Exception as e:
                 if filename:
-                    with open(filename, 'a', encoding='utf-8') as file:
-                        file.write(f'my_function error: {e}, inputs: {args}, {kwargs}')
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"my_function error: {e}, inputs: {args}, {kwargs}")
                 else:
-                    print(f'my_function error: {e}, inputs: {args}, {kwargs}')
+                    print(f"my_function error: {e}, inputs: {args}, {kwargs}")
                 raise
             return result
+
         return wrapper
+
     return decorator
 
 
-
-@log(filename='')
+@log(filename="")
 def func(x, y):
-    return x+y
+    return x + y
+
 
 result = func(1, 2)
 print(result)
-
-
