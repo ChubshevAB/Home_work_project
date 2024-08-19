c7c6e7c3141661471e7480d346ee67bb5e51ebd9
Adm <anton.chubshev@yandex.ru>
2024-08-19 15:47:08 +0300

    added logging

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,26 @@
+import logging
+import datetime
+
+logging.basicConfig(
+    filename="../logs/logs_masks.log",
+    filemode="w",
+    level=logging.DEBUG,
+    format="%(levelname)s: %(asctime)s - %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+my_logger = logging.getLogger("log_mask")
+
+
 def get_mask_card_number(user_input_card: list) -> str:
     """Функция принимает в качестве аргумента номер карты и возвращает его маску."""
 
     # Выводит сообщение, если не введен номер карты
     if not user_input_card:
+        my_logger.warning(msg="Некорректный ввод")
         return "Некорректный ввод"
 
     # Разбиваем номер карты на список из 4 элементов
-    mask = [user_input_card[i: i + 4] for i in range(0, len(user_input_card), 4)]
+    mask = [user_input_card[i : i + 4] for i in range(0, len(user_input_card), 4)]
 
     # Заменяем цифры в номере карты на знак *
     for i in range(2, len(mask[1])):  # Заменяем все кроме первых двух
@@ -17,7 +31,7 @@ def get_mask_card_number(user_input_card: list) -> str:
 
     # Собираем замаскированный номер карты
     mask_card_number = " ".join("".join(part) for part in mask)
-
+    my_logger.info(msg=f"Замаскированный номер карты: {mask_card_number}")
     return mask_card_number
 
 
@@ -26,6 +40,7 @@ def get_mask_account(user_input_account: list) -> str:
 
     # Выводит сообщение, если не введен номер счета или он не соответствует формату в 20 знаков
     if not user_input_account or len(user_input_account) != 20:
+        my_logger.warning(msg="Некорректный ввод")
         return "Некорректный ввод"
 
     # Берем последние 6 цифр из номера счета для формирования маски
@@ -37,7 +52,7 @@ def get_mask_account(user_input_account: list) -> str:
 
     # Преобразовываем получившийся список в строку для формирования окончательного вида маски
     mask_account = "".join(new_mask_account)
-
+    my_logger.info(msg=f"Замаскированный номер счета: {mask_account}")
     return mask_account
 
 
